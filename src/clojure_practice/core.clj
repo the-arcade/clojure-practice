0c47f260691ef36340d8e0dd7b903e8811da1eec
Dillon Jeffers <dillon_jeffers@fastmail.com>
2020-03-15 06:36:57 +0000

    add two versions of a factorial function

--- src/clojure_practice/core.clj
+++ src/clojure_practice/core.clj
@@ -1,4 +1,11 @@
 (ns clojure-practice.core)
 
+; get a fibonacci number
 (defn fib [n]
   (if (< n 1) 0 (if (= n 1) 1 (+ (fib (- n 1)) (fib (- n 2))))))
+
+; calculate factorials
+(defn fact-recursion [n]
+  (if (= n 0) 1 (* n (fact-recursion (- n 1)))))
+(defn fact-reduce [n]
+  (reduce * (range 2 (+ n 1))))
--- test/clojure_practice/core_test.clj
+++ test/clojure_practice/core_test.clj
@@ -9,3 +9,14 @@
     (is (= (fib 2) 1))
     (is (= (fib 3) 2))
     (is (= (fib 31) 1346269))))
+
+(deftest fact-recursion-test
+  (testing "Factorials"
+    (is (= (fact-recursion 0) 1))
+    (is (= (fact-recursion 1) 1))
+    (is (= (fact-recursion 10) 3628800))))
+(deftest fact-reduce-test
+  (testing "Factorials"
+    (is (= (fact-reduce 0) 1))
+    (is (= (fact-reduce 1) 1))
+    (is (= (fact-reduce 10) 3628800))))
